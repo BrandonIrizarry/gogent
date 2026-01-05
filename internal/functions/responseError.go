910b92c1f1b9c4e90301fe6e4490802d0da0dbf7
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-01-05 11:40:02 -0500

    docs: add doc comment to ResponseError

--- internal/functions/responseError.go
+++ internal/functions/responseError.go
@@ -2,6 +2,11 @@ package functions
 
 import "google.golang.org/genai"
 
+// ResponseError returns a Part describing an error in our (the
+// programmer's) functions (for example, trying to read from a file
+// that doesn't exist.) It accepts the name of the function call, as
+// well as the error message to be included as part of the return
+// value.
 func ResponseError(funCallName, message string) *genai.Part {
 	return genai.NewPartFromFunctionResponse(funCallName, map[string]any{
 		"error": message,
