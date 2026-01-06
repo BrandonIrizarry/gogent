cb112042fc6327f40f9a885a6ceee74b84fb5bdb
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-01-06 17:00:38 -0500

    feat: allow -dir to accept a relative path
    Also, we let the current directory be the -dir switch's default
    argument.

--- internal/cliargs/cliArguments.go
+++ internal/cliargs/cliArguments.go
@@ -1,10 +1,10 @@
 package cliargs
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 type CLIArguments struct {
@@ -15,17 +15,22 @@ type CLIArguments struct {
 
 func NewCLIArguments() (CLIArguments, error) {
 	var cliArgs CLIArguments
+	var wdir string
 
 	flag.IntVar(&cliArgs.NumIterations, "num", 20, "The number of times the function call loop should execute (defaults to 20)")
 	flag.IntVar(&cliArgs.MaxFilesize, "maxsize", 10000, "The size limit for file-reading operations (defaults to 10KB)")
-	flag.StringVar(&cliArgs.WorkingDir, "dir", "", "The top-level project directory")
+	flag.StringVar(&wdir, "dir", ".", "The top-level project directory (absolute path, else defaults to current directory)")
 
 	flag.Parse()
 
-	// Make sure the '-dir' argument was given.
-	if cliArgs.WorkingDir == "" {
-		return CLIArguments{}, errors.New("Missing '-dir' argument")
+	// Make sure we're using an absolute path, in case a relative one was given.
+	wdir, err := filepath.Abs(wdir)
+
+	if err != nil {
+		return CLIArguments{}, err
 	}
+
+	cliArgs.WorkingDir = wdir
 
 	// Make sure the provided working directory is, in fact, a
 	// directory.
