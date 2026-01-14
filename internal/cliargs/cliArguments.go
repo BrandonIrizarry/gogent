6851fa4f25430a6549bfaacaf052f2022c84238d
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-01-13 23:24:57 -0500

    fix: remove excessive "defaults to" messages

--- internal/cliargs/cliArguments.go
+++ internal/cliargs/cliArguments.go
@@ -20,8 +20,8 @@ Examples:
 -logmode debug
 -logmode info,debug
 `)
-	flag.StringVar(&cliArgs.ConfigFilename, "config", "gogent.yaml", "YAML configuration file (defaults to gogent.yaml)")
-	flag.StringVar(&cliArgs.LogFilename, "log", "logs.txt", "Path to logfile (defaults to logs.txt)")
+	flag.StringVar(&cliArgs.ConfigFilename, "config", "gogent.yaml", "YAML configuration file")
+	flag.StringVar(&cliArgs.LogFilename, "log", "logs.txt", "Path to logfile")
 
 	flag.Parse()
 
